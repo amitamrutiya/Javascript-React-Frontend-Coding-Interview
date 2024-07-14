587ed29bfe5a48dfe47bd60d36680f273ae1910b
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-14 18:40:35 +0530

    add nested file structur project
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ file-explorer/README.md
@@ -0,0 +1,3 @@
+# Nested File Structure
+
+![image](https://github.com/user-attachments/assets/929e31a8-6fe1-4d99-86f3-aa5525326475)
---
+++ file-explorer/src/App.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import Folder from "./components/folder";
+import useTraverseTree from "./hooks/use-traverse-tree";
+import "./App.css";
+import explorer from "./data/folderData";
+
+export default function App() {
+  const [explorerData, setExplorerData] = useState(explorer);
+
+  const { insertNode } = useTraverseTree();
+
+  const handleInsertNode = (folderId, item, isFolder) => {
+    const finalTree = insertNode(explorerData, folderId, item, isFolder);
+    setExplorerData(finalTree);
+  };
+
+  return (
+    <div className="App">
+      <Folder handleInsertNode={handleInsertNode} explorer={explorerData} />
+    </div>
+  );
+}
