cb12ffa385e5208d1c53258714720e5a7060431e
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-16 19:54:45 +0530

    create grid-lights project
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ grid-lights/src/components/Cell.jsx
@@ -0,0 +1,12 @@
+export default
+function Cell({ filled, onClick, isDisabled, label }) {
+  return (
+    <button
+      type="button"
+      aria-label={label}
+      disabled={isDisabled}
+      onClick={onClick}
+      className={filled ? "cell cell-activated" : "cell"}
+    />
+  );
+}
