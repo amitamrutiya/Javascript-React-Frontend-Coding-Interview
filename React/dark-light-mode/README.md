39058278e26e9a92cee5b09fff61341fe819d94e
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-17 18:48:52 +0530

    create dark-light-mode project
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ React/dark-light-mode/README.md
@@ -0,0 +1,3 @@
+# Dark Light Mode
+
+![image](https://github.com/user-attachments/assets/8b390f26-51ff-4ba4-af64-2f98c36a5114)
---
+++ React/dark-light-mode/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./context/theme-context";
+import Navbar from "./components/navbar";
+import About from "./pages/about";
+import Blog from "./pages/blog";
+import Home from "./pages/home";
+
+import "./index.css";
+
+const App = () => {
+  return (
+    <ThemeProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+};
+
+export default App;
