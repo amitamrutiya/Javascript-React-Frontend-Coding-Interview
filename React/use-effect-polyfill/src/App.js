e0f8b6166e273640eefdc4e5091015ca2c7267a5
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-17 18:14:16 +0530

    create use-effect-polyfill project
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ React/use-effect-polyfill/src/App.js
@@ -0,0 +1,11 @@
+import Counter from "./components/counter";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Amit Amrutiya</h1>
+      <h2>Follow for 69 Years of Goodluck</h2>
+      <Counter />
+    </div>
+  );
+}
