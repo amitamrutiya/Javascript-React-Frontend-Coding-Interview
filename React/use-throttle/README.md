9125280cf57f7f68c238c76e3652cce69598c29e
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-18 23:00:17 +0530

    create use-throttle hook
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ React/use-throttle/README.md
@@ -0,0 +1,1 @@
+# Use Throttle Hook
---
+++ React/use-throttle/src/App.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import "./index.css";
+import useThrottle from "./hooks/use-throttle.js";
+
+function App() {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const handleResize = () => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+
+    // any expensive operation or API Call
+  };
+
+  const throttledHandleResize =  useThrottle(handleResize, 1000);
+
+  useEffect(() => {
+    window.addEventListener("resize", throttledHandleResize);
+
+    return () => {
+      window.removeEventListener("resize", throttledHandleResize);
+    };
+  }, []);
+
+  return (
+    <div>
+      Window Size: {windowSize.width} x {windowSize.height}
+    </div>
+  );
+}
+
+export default App;
