581fb00b716813cd1aa2a7b7e7af2c4e664942dd
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-21 22:26:37 +0530

    create tic-tac-toe project
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ React/tic-tac-toe/README.md
@@ -0,0 +1,3 @@
+# Tic Tac Toe
+
+![image](https://github.com/user-attachments/assets/9e0a28f8-b7da-4dc2-bd58-b93c13fc3529)
---
+++ React/tic-tac-toe/src/App.js
@@ -0,0 +1,7 @@
+import TicTacToe from "./components/tic-tac-toe.js";
+
+function App() {
+  return <TicTacToe boardSize={4} />;
+}
+
+export default App;
