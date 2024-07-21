19a4d4a508e5c9642532ebf77f340d1d59d1e9ea
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-21 20:53:09 +0530

    create curreny converter project
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ React/currency-converter/README.md
@@ -0,0 +1,3 @@
+# Currency Converter
+
+![image](https://github.com/user-attachments/assets/3d086ca0-4ed2-448c-af39-6ab8c7d045d8)
---
+++ React/currency-converter/src/App.js
@@ -0,0 +1,13 @@
+import CurrencyConvertor from "./components/currency-convertor.js";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+      <div className="container">
+        <CurrencyConvertor />
+      </div>
+    </div>
+  );
+}
+
+export default App;
