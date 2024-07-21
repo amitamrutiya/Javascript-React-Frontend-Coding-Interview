058a0f8642e13952b565a4baa7a43b63557cc820
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-21 12:28:08 +0530

    create selectable grid project
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ React/selectable-grid/README.md
@@ -0,0 +1,3 @@
+# Selectable Grid
+
+![image](https://github.com/user-attachments/assets/eb5630cb-18ed-4ef1-861d-badf64e2c941)
---
+++ React/selectable-grid/src/App.js
@@ -0,0 +1,12 @@
+import SelectableGrid from "./components/selectable-grid";
+
+function App() {
+  return (
+    <div>
+      <h1>Selectable Grid</h1>
+      <SelectableGrid rows={10} cols={10} />
+    </div>
+  );
+}
+
+export default App;
