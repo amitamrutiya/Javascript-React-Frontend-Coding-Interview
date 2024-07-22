84575a57064fb8065a9ac5f984489c493cf84756
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-22 16:15:16 +0530

    create nested-comment project
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ React/nested-comment/README.md
@@ -0,0 +1,3 @@
+# Nested Comment using tree travesal
+
+![image](https://github.com/user-attachments/assets/b8d59277-edd1-4d07-80d6-a3b4c831a5e6)
---
+++ React/nested-comment/src/App.js
@@ -0,0 +1,20 @@
+import NestedComments from "./components/nested-comments.js";
+import commentsData from "./data/comments.json";
+
+function App() {
+  return (
+    <div>
+      <h1>Nested Comment System</h1>
+      <NestedComments
+        comments={commentsData}
+        onSubmit={() => {}}
+        onEdit={() => {}}
+        onDelete={() => {}}
+        // onUpvote={() => {}}
+        // onDownvote={() => {}}
+      />
+    </div>
+  );
+}
+
+export default App;
