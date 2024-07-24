b85a8aa7a6447ae58c108e9baae2ad13eed6f86f
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-24 21:07:56 +0530

    create list-reorder component
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ React/list-reorder-dnd/README.md
@@ -0,0 +1,3 @@
+# List Reorder DnD
+
+![image](https://github.com/user-attachments/assets/57840aca-3a0b-487f-ace2-62ac5570bc10)
---
+++ React/list-reorder-dnd/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DragAndDropList from "./components/DragAndDropList";
+
+const App = () => {
+  return (
+    <div className="App">
+      <DragAndDropList />
+    </div>
+  );
+};
+
+export default App;
