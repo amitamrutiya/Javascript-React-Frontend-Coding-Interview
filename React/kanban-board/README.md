092e7a947acd795d07e03c2b702b3ed5f491987c
Amit Amrutiya <amitamrutiya2210@gmail.com>
2024-07-24 21:13:26 +0530

    create kanban board project
    Signed-off-by: Amit Amrutiya <amitamrutiya2210@gmail.com>

---
+++ React/kanban-board/README.md
@@ -0,0 +1,3 @@
+# Kanban Board
+
+![image](https://github.com/user-attachments/assets/b097cd8a-535f-4ad8-8698-191dd9472619)
---
+++ React/kanban-board/src/components/TaskForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const TaskForm = ({ addTask }) => {
+  const [taskContent, setTaskContent] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!taskContent.trim()) return;
+    addTask(taskContent);
+    setTaskContent('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={taskContent}
+        onChange={(e) => setTaskContent(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button type="submit">Add Task</button>
+    </form>
+  );
+};
+
+export default TaskForm;
